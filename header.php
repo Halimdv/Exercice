d1353bba7a36a5a1209af4059d291105d854d92d
Halimdv <deville.halim@gmail.com>
2016-07-01 11:15:06 +0200

    Adding jquery and using altorouter in index.php

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>35 git</title>
+	<link rel="stylesheet" href="css/script.css">
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,6 +1,36 @@
 <?php
 
+require_once 'vendor/autoload.php';
+require_once 'header.php';
+if(file_exists('config.php')){
+	require_once 'config.php';
+} else {
+	echo "Vous devez configurer votre base de données.";
+}
+
+$router = new AltoRouter();
+$router->setBasePath('/cours/35git');
+
+echo "test";
+$router->map('GET','/',function(){
+	echo "Hello world";
+	
+});
+
+$router->map('GET','/[:slug]',function($slug){
+	echo "Hello ".$slug;
+});
+
+$match = $router->match();
+
+if($match && is_callable($match['target'])){
+	//echo "La page existe";
+	//echo $match['target']($match['params']);
+	call_user_func_array($match['target'], $match['params']);
+} else {
+	echo "404";
+}
 
 
-
+require_once 'footer.php';
 ?>
